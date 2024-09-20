5f02b2556e374276179af0b4fc5fb0bcc7f2b2ba
EshaarveerSingh <sineshaa@sheridancollege.ca>
2024-09-19 20:13:59 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,7 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
-
+/**
+ * A class that fills a magic hand of 7 cards with random Card Objects
+ * and then asks the user to pick a card and searches the array of cards
+ * for the match to the user's card. To be used as starting code in ICE 1
+ * @author Eshaarveer Singh
+ */
 import java.util.Random;
 import java.util.Scanner;
 
